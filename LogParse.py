301b747d9d8d54a61090750cf5c902cefaebaa39
Andrew Stuart <narayn60@gmail.com>
2015-01-29 17:32:38 +0000

    Updated to return dictionary result

--- LogParse.py
+++ LogParse.py
@@ -4,30 +4,27 @@
 
 from bs4 import BeautifulSoup
 
+
 class LogParse:
 
-    def main(self):
-        file = open("./nvdcve-2.0-2015.xml", "r")
+    def main(self, file_name):
+        file = open(file_name, "r")
         soup = BeautifulSoup(file)
         parts = soup.findAll("entry")
         self.extractEntries(parts)
-
-    def extractEntries(self, xmlSoup):
-        for entry in xmlSoup:
+        for entry in parts:
+            dict_result = {}
             part_attrs = dict(entry.attrs)
-            part_id = part_attrs['id']
-            print(part_id)
-            self.vulnerableSoftwareList(entry)
-
-    def vulnerableSoftwareList(self, xmlSoup):
-        softwareList = xmlSoup.findAll("vuln:vulnerable-software-list")
-        for software in softwareList:
-            temps = software.findAll("vuln:product")
-            for temp in temps:
-                print(str(temp.contents)[9:-2])
-
+            dict_result['CVE'] = str(part_attrs['id'])[4:]
+            dict_result['vulnerableSoftware'] = {}
+            softwareList = entry.findAll("vuln:vulnerable-software-list")
+            for software in softwareList:
+                temps = software.findAll("vuln:product")
+                for i in range(len(temps)):
+                    dict_result['vulnerableSoftware'][i] = str(temps[i].contents)[9:-2]
+            print(dict_result)
 
 if __name__ == "__main__":
     logParser = LogParse()
-    logParser.main()
+    logParser.main("./nvdcve-2.0-2015.xml")
 
